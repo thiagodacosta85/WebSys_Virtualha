0a2595607a6e779aaf6e34b6734c618d9c462c7a
Thiago da Costa <thiago.costa85@outlook.com>
2024-05-24 17:14:28 -0300

    Construcao de graficos de distribuicao populacional

--- dataAnalysis/r/distribuicaoPopulacional.R
+++ dataAnalysis/r/distribuicaoPopulacional.R
@@ -1,5 +1,7 @@
 setwd("C:/llpDigital/WebSys_Virtualha/dataAnalysis")
 
+library(RColorBrewer)
+
 localidades <- read.csv("C:/llpDigital/WebSys_Virtualha/dataAnalysis/data/localidades.csv", header=TRUE, sep = ";", fileEncoding = 'latin1')
 localidades$DATA_FUND <- as.Date(localidades$DATA_FUND, format = "%Y-%m-%d")
 View(localidades)
@@ -7,8 +9,24 @@ View(localidades)
 str(localidades)
 head(localidades)
 
+
+
+#-->passando cores da paleta RColorBrewer<--
+#brewer.pal.info
+#color <- brewer.pal(09, "Reds")
+#par(mar=c(14,4,2,1))
+#barplot(height = localidades$POPULACAO, names = localidades$NOME, col = color, las=2)
+#box()
+
 par(mar=c(14,4,2,1))
-barplot(height = localidades$POPULACAO, names = localidades$NOME, col = blues9, las=2)
+barplot(height = localidades$POPULACAO, names = localidades$NOME, main = "Distribuição Populacional",
+        col = "cornflowerblue", las=2)
 box()
+
+par(mar=c(4,14,1,2))
+barplot(height = localidades$POPULACAO, names = localidades$NOME, main = "Distribuição Populacional",
+        col = "cornflowerblue", horiz = TRUE, las=2)
+box()
+
 
 
