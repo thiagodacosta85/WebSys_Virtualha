dc65210c373d91b7af0731095eb24fafd3650fa1
Thiago da Costa <thiago.costa85@outlook.com>
2024-06-05 19:43:56 -0300

    atualizacao de greficos e insercao das camadas de labels

--- dataAnalysis/r/distribuicaoPopulacional.R
+++ dataAnalysis/r/distribuicaoPopulacional.R
@@ -10,6 +10,8 @@ if(!require(tidyverse))
   install.packages("tidyverse")
 library(tidyverse)
 
+
+
 # </setup>
 
 # <data>
@@ -40,62 +42,90 @@ localidades %>% select(NOME, ZONA, POPULACAO, ATIV)
 
 # </dataCheck>
 
+# <manipulations>
+
+localGruposRes <- select(localidades, NOME, ZONA, POPULACAO, CRIANCAS, ADOLESC,
+                         JOVENS_ADUL, ADULTOS, IDOSOS)
+
+localGrupos  <-  pivot_longer(localGruposRes,c(
+  CRIANCAS, ADOLESC, JOVENS_ADUL, ADULTOS, IDOSOS),
+  names_to = "Grupo",
+  values_to = "Quantidade")
+rm(localGruposRes)
+view(localGrupos)
+
+# </manipulations>
+
 # <charts>
 
   # <barplotDistribuicaoPopulacional>
 
-  ggplot(localidades, aes(x = NOME, y = POPULACAO, fill = ZONA)) +
+  ggplot(localidades, aes(x = NOME, y = POPULACAO, fill = ZONA, label = localidades$POPULACAO)) +
     geom_bar(stat = 'identity') +
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
           axis.text.y = element_text(angle = 0, vjust = 0.5, hjust = 1)) +
     labs(x = "Localidade",
          y = "População", 
          title = "Distribuição Populacional", 
-         subtitle = NULL)
+         subtitle = NULL) +
+    geom_label(size = 3, fill="gray95")
   
   # </barplotDistribuicaoPopulacional>
   
+  # <barplotDistribuicaoPopulacionalPorGrupo>
+  
+  ggplot(localGrupos, aes(x = NOME, y = Quantidade, fill = Grupo)) +
+    geom_bar(stat = 'identity') +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+          axis.text.y = element_text(angle = 0, vjust = 0.5, hjust = 1)) +
+    labs(x = "Localidade",
+         y = "População", 
+         title = "Distribuição Populacional", 
+         subtitle = "Por grupo de individuos")
+  
+  # </barplotDistribuicaoPopulacionalPorGrupo>
+  
   # <barplotValorMedioTerreno>
   
-  ggplot(localidades, aes(x = NOME, y = VALOR_MED_TERRENO)) +
-    geom_bar(stat = 'identity', fill = "cornflowerblue") +
+  ggplot(localidades, aes(x = NOME, y = VALOR_MED_TERRENO, fill = ZONA, label = VALOR_MED_TERRENO)) +
+    geom_bar(stat = 'identity') +
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
           axis.text.y = element_text(angle = 0, vjust = 0.5, hjust = 1)) +
     labs(x = "Localidade",
          y = "Valor Médio", 
-         title = "Valor médio dos terrnos por localidade", 
-         subtitle = NULL)
+         title = "Valor médio dos terrenos por localidade", 
+         subtitle = NULL) +
+    geom_label(size = 3, fill="gray95")
+  
+  
   # </barplotValorMedioTerreno>
+  
+  # <barplotLocalidadesPorZona>
+  
+  localidades %>%
+    group_by(ZONA) %>%
+    summarise(
+      contador = n()
+    ) %>%
+    ggplot(aes(ZONA,contador,label=contador, fill=ZONA))+
+    geom_bar(stat = "identity")+
+    labs(title="Localidades por Zona",
+         subtitle = NULL,
+         x="Zona",
+         y= NULL)+
+    theme(axis.text.x = element_blank()) +
+    theme(axis.ticks.x.bottom = element_blank()) +
+    geom_label(size = 4, fill="gray95") + 
+    coord_flip()
+  
+  # </barplotLocalidadesPorZona>
+  
   
   # </charts>
   
-  # <manipulations>
     
-    localGruposRes <- select(localidades, NOME, ZONA, POPULACAO, CRIANCAS, ADOLESC,
-                          JOVENS_ADUL, ADULTOS, IDOSOS)
-  
-    localGrupos  <-  pivot_longer(localGruposRes,c(
-                                  CRIANCAS, ADOLESC, JOVENS_ADUL, ADULTOS, IDOSOS),
-                                  names_to = "Grupo",
-                                  values_to = "Quantidade")
-    rm(localGruposRes)
-    view(localGrupos)
-  
-    # </manipulations>
     
-    # <charts02>
-      
-    ggplot(localGrupos, aes(x = NOME, y = Quantidade, fill = Grupo)) +
-      geom_bar(stat = 'identity') +
-      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
-            axis.text.y = element_text(angle = 0, vjust = 0.5, hjust = 1)) +
-      labs(x = "Localidade",
-           y = "População", 
-           title = "Distribuição Populacional", 
-           subtitle = "Por grupo de individuos")
-    
-    # </charts02>
     
 
-    
+
   
